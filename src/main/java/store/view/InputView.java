0b9a75f8a1987928daff9894eccacb917c2dae49
na0th <105775683+na0th@users.noreply.github.com>
2024-11-08 14:16:46 +0900

    feat : View 클래스 추가
    추가한 내용 :
    1.View를 Input, Output으로 분리
    2. View에 맞는 메서드 추가

---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,32 @@
+package store.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String getProducts() {
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1]");
+        return Console.readLine();
+    }
+
+    public String getMembershipDiscount() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
+
+    public String getWantAnotherProducts() {
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        return Console.readLine();
+    }
+
+    public String getConfirmPurchaseWithoutPromotion() {
+        System.out.println("현재 콜라 4개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
+
+    public String getConfirmAddFreeProducts() {
+        System.out.println("현재 오렌지주스은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
+
+}
---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,14 @@
+package store.view;
+
+public class OutputView {
+    public void printProducts() {
+        System.out.println("안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.");
+    }
+
+    public void printReceipt() {
+        System.out.println("=====W 편의점=====");
+        System.out.println("상품명\t\t수량\t금액");
+    }
+
+}
