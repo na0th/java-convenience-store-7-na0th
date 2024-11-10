b60106ce87eddd02f8f4358972f92973635613c3
na0th <105775683+na0th@users.noreply.github.com>
2024-11-10 18:56:00 +0900

    feat : WareHouse 클래스 및 단위 테스트 추가
    추가한 내용 :
    1. WareHouse 클래스
    -> DB 역할을 하면서, Reposiory 역할로 설정
    재고 처리 Strategy에 따라서 동적으로 메서드 수행
    
    2. processStock 메서드가 제대로 DTO를 반환하는지 테스트했음
    
    미구현 :
    프로모션 처리 전략일 때 메서드 미구현
    프로모션 처리 전략일 때 메서드 테스트 미구현

---
+++ src/test/java/store/model/WareHouseTest.java
@@ -0,0 +1,88 @@
+package store.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.dto.response.ReceiptSingleDto;
+import store.model.stockProcessStrategy.RegularStockStrategy;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WareHouseTest {
+
+    private WareHouse wareHouse;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Product> products = new LinkedHashMap<>();
+
+        Product product1 = Product.create(
+                "testProduct1",
+                1000,
+                10,
+                10,
+                "testPromotionName1"
+        );
+        Product product2 = Product.create(
+                "testProduct2",
+                1000,
+                10,
+                10,
+                "testPromotionName2"
+        );
+
+        products.put(product1.getName(), product1);
+        products.put(product2.getName(), product2);
+
+        wareHouse = new WareHouse(products);
+    }
+
+    @Test
+    @DisplayName("findByProductName 메서드 테스트")
+    void productName으로_Product를_찾으면_성공한다() {
+        // Given
+        String productName = "testProduct1";
+        // When
+        Product result = wareHouse.findByProductName(productName);
+        // Then
+        assertEquals(productName, result.getName());
+    }
+
+    @Test
+    @DisplayName("findByProductName 메서드 테스트")
+    void productName으로_Product가_없으면_예외_발생() {
+        // given
+        String productName = "testProduct3";
+        // when & then
+        assertThatThrownBy(() -> wareHouse.findByProductName(productName))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    @DisplayName("processStock 메서드 테스트")
+    void 재고_전략으로_처리된_결과_DTO를_리턴받으면_성공한다() {
+        // given
+        Product product = wareHouse.findByProductName("testProduct1");
+        int expectedDeductedQuantity = 6;
+        int expectedRemainderQuantity = 4;
+        RegularStockStrategy regularStockStrategy = new RegularStockStrategy();
+
+        // when
+        ReceiptSingleDto result = wareHouse.processStock(product, 6, regularStockStrategy);
+
+        // then
+        assertNotNull(result);
+        assertEquals(expectedRemainderQuantity, product.getRegularStock());
+        assertEquals(10,product.getPromotionStock());
+
+        assertEquals("testProduct1", result.getProductName());
+        assertEquals(expectedDeductedQuantity, result.getRegularStockDeducted());
+        assertEquals(0, result.getPromotionStockDeducted());
+    }
+}
