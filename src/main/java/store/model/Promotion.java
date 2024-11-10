7094045428a2625a7c2af28d28d50e61b44573d8
na0th <105775683+na0th@users.noreply.github.com>
2024-11-11 01:11:58 +0900

    feat : Promotion 메서드 및 단위 테스트 추가
    1. Promotion 클래스 메서드 및 단위 테스트 추가

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 
 public class Promotion {
     //TODO 여기서 프로모션에 있는지 확인할 수 있지 않을까?
+    private static final int ONE_FREE_ITEM = 1;
+    private static final int NO_FREE_ITEM = 0;
+
 
     private String promotionName;
     //(buy,get) , (startDate,endDate)는 묶는 걸 고려
@@ -28,6 +31,24 @@ public class Promotion {
         return (currentDate.isEqual(startDate) || currentDate.isAfter(startDate)) &&
                 (currentDate.isEqual(endDate) || currentDate.isBefore(endDate));
     }
+    //테스트 필수
+    public int calculateFreeItem(int quantity) {
+        int buyCount = Integer.parseInt(buy);
+        int getCount = Integer.parseInt(get);
+
+        int group = buyCount + getCount;
+        int remainder = quantity % group;
+
+        if (buyCount <= 0 || getCount <= 0) {
+            throw new IllegalArgumentException();
+        }
+        if ((remainder % buyCount == 0) && remainder > 0) {
+            return ONE_FREE_ITEM;
+        }
+
+        return NO_FREE_ITEM;
+    }
+
     public boolean hasPromotionName(String name) {
         return promotionName.equals(name);
     }
--- src/test/java/store/model/PromotionTest.java
+++ src/test/java/store/model/PromotionTest.java
@@ -1,5 +1,6 @@
 package store.model;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class PromotionTest {
 
     private Promotion promotion;
+    private Promotion promotion2;
+    private Promotion promotion3;
 
     @BeforeEach
     void setUp() {
         promotion = new Promotion(
                 "testPromotion",
                 "1",
+                "1",
+                LocalDate.of(2024, 11, 1),
+                LocalDate.of(2024, 12, 31));
+
+        promotion2 = new Promotion(
+                "testPromotion2",
+                "2",
+                "1",
+                LocalDate.of(2024, 11, 1),
+                LocalDate.of(2024, 12, 31));
+        promotion3 = new Promotion(
+                "testPromotion3",
+                "3",
                 "1",
                 LocalDate.of(2024, 11, 1),
                 LocalDate.of(2024, 12, 31));
@@ -37,4 +53,40 @@ class PromotionTest {
         assertEquals(Boolean.FALSE, promotion.isWithinValidDate(LocalDate.of(2025, 11, 10)));
     }
 
+    @Test
+    @DisplayName("calculateFreeItem 메서드 테스트")
+    void 빠뜨린_프로모션_재고를_제대로_리턴하면_성공한다() {
+        //given
+        int quantity = 7;
+        //when
+
+        //then
+        Assertions.assertThat(1).isEqualTo(promotion.calculateFreeItem(quantity));
+        Assertions.assertThat(0).isEqualTo(promotion2.calculateFreeItem(quantity));
+        Assertions.assertThat(1).isEqualTo(promotion3.calculateFreeItem(quantity));
+    }
+    @Test
+    @DisplayName("calculateFreeItem 메서드 테스트")
+    void 빠뜨린_프로모션_재고를_제대로_리턴하면_성공한다2() {
+        //given
+        int quantity = 9;
+        //when
+
+        //then
+        Assertions.assertThat(1).isEqualTo(promotion.calculateFreeItem(quantity));
+        Assertions.assertThat(0).isEqualTo(promotion2.calculateFreeItem(quantity));
+        Assertions.assertThat(0).isEqualTo(promotion3.calculateFreeItem(quantity));
+    }
+    @Test
+    @DisplayName("calculateFreeItem 메서드 테스트")
+    void 빠뜨린_프로모션_재고를_제대로_리턴하면_성공한다3() {
+        //given
+        int quantity = 11;
+        //when
+
+        //then
+        Assertions.assertThat(1).isEqualTo(promotion.calculateFreeItem(quantity));
+        Assertions.assertThat(1).isEqualTo(promotion2.calculateFreeItem(quantity));
+        Assertions.assertThat(1).isEqualTo(promotion3.calculateFreeItem(quantity));
+    }
 }
