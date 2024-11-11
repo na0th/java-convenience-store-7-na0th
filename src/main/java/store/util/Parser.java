568b940533d21463211fb5add69cfdfb559c5ab2
na0th <105775683+na0th@users.noreply.github.com>
2024-11-11 23:24:12 +0900

    fix : 0이하 숫자 검증 메서드 추가
    예시)[사이다-0]

--- src/main/java/store/util/Parser.java
+++ src/main/java/store/util/Parser.java
@@ -52,8 +52,16 @@ public class Parser {
 
     private static int parseQuantity(String quantityStr, String productName) {
         try {
-            return Integer.parseInt(quantityStr);
+            int quantity = Integer.parseInt(quantityStr);
+            validateIsPositiveQuantity(quantity);
+            return quantity;
         } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static void validateIsPositiveQuantity(int quantity) {
+        if (quantity <= 0) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         }
     }
